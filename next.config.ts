a8379406b691ad4da63bcfbcde0b2b7e49088b9e
Tahjay Ulett <tahjayulett@gmail.com>
2025-09-04 10:55:30 -0500

    Bugfix

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,8 @@ import type { NextConfig } from 'next'
 const nextConfig: NextConfig = {
   output: 'export',
   trailingSlash: true,
+  basePath: '/sadralindsay', // Replace with your actual repo name
+  assetPrefix: '/sadralindsay',
   images: {
     unoptimized: true
   }
